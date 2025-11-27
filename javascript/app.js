5787b314fe4c40736d137452ff9f3a03d4974605
Marc MOSCA <marc.mosca@icloud.com>
2025-11-27 12:18:18 +0100

    Ajout de la structure JS et du CSS du terminal

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!")
+const output = document.getElementById("output")
+const input = document.getElementById("input")
+const form = document.getElementById("form")
+const prompt = document.getElementById("prompt")
+
+let history = JSON.parse(localStorage.getItem("history") || "[]")
+let historyIndex = null
---
+++ javascript/constants.js
@@ -0,0 +1,19 @@
+const skills = [
+  ["Langages", "PHP, TypeScript, HTML, CSS, C, C++, Swift, SQL"],
+  ["Outils", "Git, Docker, Linux, MacOS, Helix"]
+]
+
+const projects = [
+  {
+    id: "marc-mosca.fr",
+    name: "marc-mosca.fr",
+    description: "Dépôt de mon portfolio.",
+    link: "https://github.com/marc-mosca/marc-mosca.fr"
+  },
+  {
+    id: "dotfiles",
+    name: "dotfiles",
+    description: "Dépôt regroupant mes configurations système et logiciel.",
+    link: "https://github.com/marc-mosca/dotfiles"
+  }
+]
